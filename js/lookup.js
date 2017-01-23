c68e1fb50dbbf34ca9691b6b2a6bc69b3d9f86b6
james howard <james@>
2017-01-23 15:04:57 -0800

    401 unauthorized error

--- js/lookup.js
+++ js/lookup.js
@@ -1,17 +1,22 @@
 var apiKey = require('./../.env').apiKey;
 
-Search = function(doctors){
-  this.name = doctors;
+Search = function(){;
 };
 
-Search.prototype.getDoctors = function(medicalIssue, displayFunction) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+  medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=cf6d046cadf9d854c09e021fb320f513').then(function(result) {
-    displayFunction(medicalIssue, result.doctors);
-      console.log(result);
-    }).fail(function(error){
-      $('.showDoctors').text("error");
-      console.log("fail");
-    });
+Search.prototype.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+  medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=cf6d046cadf9d854c09e021fb320f513' + apiKey).then(function(result) {
+    for(i = 0; i < 5; i++) {
+      var firstName = result.data[i].Profile.first_name;
+      var middleName = result.data[i].Profile.middle_name;
+      var firstName = result.data[i].Profile.first_name;
+
+      return result
+    }
+
+  }).fail(function(error){
+    $('.showDoctors').text("error");
+    console.log("fail");
+  });
 };
 
 exports.searchModule = Search;
